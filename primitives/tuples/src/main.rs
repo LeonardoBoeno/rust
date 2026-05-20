bdef3e5dd54cd5d97a0d072af89ce07f4eb16777
Leonardo Boeno <leonardo25godoi@gmail.com>
2026-05-20 20:38:38 -0300

    Challenge: implement a display

--- primitives/tuples/src/main.rs
+++ primitives/tuples/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Formatter, Display};
+
 // Tuples can be used as function arguments and as return values.
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
     // `let` can be used to bind the members of a tuple to variables.
@@ -7,8 +9,18 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
 }
 
 // The following struct is for the activity.
-#[derive(Debug)]
+
 struct Matrix(f32, f32, f32, f32);
+
+impl Display for Matrix {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f, 
+            "( {} {} )\n( {} {} )", 
+            self.0, self.1, self.2, self.3
+        )
+    }
+}
 
 fn main() {
     // A tuple with a bunch of different types.
@@ -49,5 +61,5 @@ fn main() {
     println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
 
     let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    println!("{:?}", matrix);
+    println!("{}", matrix);
 }
