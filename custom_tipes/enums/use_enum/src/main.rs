b33df98a843d9f4f91ef669bea040611230c0927
Leonardo Boeno <leonardo25godoi@gmail.com>
2026-06-09 20:23:07 -0300

    Fix paths and study use

--- custom_tipes/enums/use_enum/src/main.rs
+++ custom_tipes/enums/use_enum/src/main.rs
@@ -1,68 +1,37 @@
-// Create an `enum` to classify a web event. Note how both
-// names and type information together specify the variant:  
-// `PageLoad != PageUnload` and `KeyPress(char) != Paste(String)`.
+// An attribute to hide warnings for unused code.
+#![allow(dead_code)]
 
-// Each is different and independent.
-enum WebEvent {
-    // An `enum` variant may either be `unit-like`,
-    PageLoad,
-    PageUnload,
-    // like tuple structs,
-    KeyPress(char),
-    Paste(String),
-    // or c-like structures.
-    Click { x: i64, y: i64 },
+enum Stage {
+    Beginner,
+    Advanced,
 }
 
-// A function which takes a `WebEvent` enum as an argument and
-// returns nothing.
-fn inspect(event: WebEvent) {
-    match event {
-        WebEvent::PageLoad => println!("page loaded"),
-        WebEvent::PageUnload => println!("page unloaded"),
-        // Destructure `c` from inside the `enum` variant.
-        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
-        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
-        // Destructure `Click` into `x` and `y`.
-        WebEvent::Click { x, y } => {
-            println!("clicked at x={}, y={}.", x, y);
-        },
-    }
-}
-
-
-enum VeryVerboseEnumOfThingsToDoWithNumbers {
-    Add,
-    Subtract
-}
-
-type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
-
-impl VeryVerboseEnumOfThingsToDoWithNumbers {
-    fn run(&self, x: i32, y: i32) -> i32 {
-        match self {
-            Self::Add => x + y,
-            Self::Subtract => x - y,
-        }
-    }
+enum Role {
+    Student,
+    Teacher,
 }
 
 fn main() {
-    let pressed = WebEvent::KeyPress('x');
-    // `to_owned()` creates an owned `String` from a string slice.
-    let pasted  = WebEvent::Paste("my text".to_owned());
-    let click   = WebEvent::Click { x: 20, y: 80 };
-    let load    = WebEvent::PageLoad;
-    let unload  = WebEvent::PageUnload;
-    let x       = Operations::Add;
-    let x2      = Operations::Subtract;
-    inspect(pressed);
-    inspect(pasted);
-    inspect(click);
-    inspect(load);
-    inspect(unload);
+    // Explicitly `use` each name so they are available without
+    // manual scoping.
+    use Stage::{Beginner, Advanced};
+    // Automatically `use` each name inside `Role`.
+    use Role::*;
 
-    
-    println!("{}", x.run(10, 5)); // 15
-    println!("{}", x2.run(10, 5)); // 15
+    // Equivalent to `Stage::Beginner`.
+    let stage = Beginner;
+    // Equivalent to `Role::Student`.
+    let role = Student;
+
+    match stage {
+        // Note the lack of scoping because of the explicit `use` above.
+        Beginner => println!("Beginners are starting their learning journey!"),
+        Advanced => println!("Advanced learners are mastering their subjects..."),
+    }
+
+    match role {
+        // Note again the lack of scoping.
+        Student => println!("Students are acquiring knowledge!"),
+        Teacher => println!("Teachers are spreading knowledge!"),
+    }
 }
